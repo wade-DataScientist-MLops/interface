bd44760815e249691ce23f2802afc6ee6fcdf3e3
wade-DataScientist-MLops <mawad5@ulaval.ca>
2025-08-26 21:20:15 -0400

    depot

---
+++ interface/src/components/DashboardElavira.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardElavira = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur le tableau de bord d'Elavira !</h1>
+      <p>Ici, vous verrez le contenu spécifique à Elavira.</p>
+      {/* Ajoutez d'autres éléments d'interface propres à Elavira */}
+    </div>
+  );
+};
+
+export default DashboardElavira;
---
+++ interface/src/components/Dashboardsolenys.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardSolenys = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur le tableau de bord de Solenys !</h1>
+      <p>Ici, vous verrez le contenu spécifique à Solenys.</p>
+      {/* Ajoutez d'autres éléments d'interface propres à Solenys */}
+    </div>
+  );
+};
+
+export default DashboardSolenys;
